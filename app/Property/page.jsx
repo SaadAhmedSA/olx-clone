c2992027e1d8b17cffa740012af14ae00c8f9919
Saad Ahmed <153908155+SaadAhmedSA@users.noreply.github.com>
2025-04-17 07:23:03 +0500

    Update page.jsx

--- app/Property/page.jsx
+++ app/Property/page.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-[100vh] flex justify-center items-center'>Property</div>
+   <div>
+       <div className="flex justify-start items-center gap-8 px-10 pt-3">
+        <Link href={"/"}>
+          <Image src={logo} alt="brand logo" width={80} />
+        </Link>
+        <Link href={"/motor"} className="flex gap-3 items-center">
+          <FaCar />
+          Motors
+        </Link>
+        <Link href={"/Property"} className="flex gap-3 items-center">
+        <BsFillHousesFill />
+          Property
+        </Link>
+      </div>
+     <div>Property</div>
+   </div>
   )
 }
 
